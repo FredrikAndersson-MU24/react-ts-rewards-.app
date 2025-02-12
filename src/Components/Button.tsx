286a20dc01d3f14e8d35a896b74dade5438b7813
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-02-12 15:39:30 +0100

    Add optional type

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,11 +2,16 @@ import { ReactEventHandler } from "react";
 
 export interface ButtonProps {
     text: string;
-    onclick: ReactEventHandler;
+    onclick?: ReactEventHandler;
+    type?: "submit" | undefined;
 }
 
 const Button = (props: ButtonProps) => {
-    return <button onClick={props.onclick}>{props.text}</button>;
+    return (
+        <button onClick={props.onclick} type={props.type}>
+            {props.text}
+        </button>
+    );
 };
 
 export default Button;
